43eb04301a05093693143027ee37ad2d2f2999df
Niall Bunting <niall.bunting@bjss.com>
2019-09-16 17:49:29 +0100

    Initial commit of the project outline
    Add an outline of the project and how it will work.

---
+++ app.js
@@ -0,0 +1,16 @@
+function DoSubmit() {
+  console.log(brokers);
+}
+
+
+function calculateYearlyCost(broker, userdetails) {
+
+}
+
+function displayBroker(broker) {
+// Write a broker to the screen
+}
+
+function displayClearBrokers() {
+// Remove all the brokers displayed
+}
---
+++ brokers.js
@@ -0,0 +1,29 @@
+var brokers = [
+  {
+    "name": "test",
+    "servicenotes": "This is a test broker",
+    "selfmanagementrank": 2,
+    
+    "platforms": [
+      {
+       "type": "app",
+       "easeofuserank": 2,
+       "designrank": 3,
+       "link": "http://example.com",
+       "note": "only on android"
+      }
+    ],
+    
+    "fees": [
+      {
+        "type": "percentage",
+        "amount": 0.9,
+        "min": 0,
+        "max": 123123,
+        "trigger": "annual",
+        "notes": "Some notes",
+        "waved": 2
+      }
+    ]
+  }
+]
